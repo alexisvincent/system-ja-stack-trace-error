6bdfe81449bb0b2e8d25fac8d7a55984a2a5c795
Alexis Vincent <alexisjohnvincent@gmail.com>
2016-03-26 12:46:32 +0200

    Updted for css issue

--- config.js
+++ config.js
@@ -17,6 +17,7 @@ System.config({
     "babel": "npm:babel-core@5.8.38",
     "babel-runtime": "npm:babel-runtime@5.8.38",
     "core-js": "npm:core-js@1.2.6",
+    "css": "github:systemjs/plugin-css@0.1.20",
     "github:jspm/nodelibs-assert@0.1.0": {
       "assert": "npm:assert@1.3.0"
     },
